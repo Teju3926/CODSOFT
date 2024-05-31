bc9581f4abadf067570b09e0eda0c0ce0fc60dc3
Teju3926 <tyannam@gitam.in>
2024-05-31 17:58:20 +0530

    Grade.java

---
+++ Grade.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Grade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int n = scanner.nextInt();
+        int[] marks = new int[n];
+        for (int i = 0; i < n; i++) {
+            int mark;
+            while (true) {
+                System.out.print("Enter marks for subject " + (i + 1) + " (0-100): ");
+                mark = scanner.nextInt();
+                if (mark >= 0 && mark <= 100) {
+                    marks[i] = mark;
+                    break;
+                } else {
+                    System.out.println("Invalid input. Marks should be between 0 and 100. Please try again.");
+                }
+            }
+        }
+        int total = 0;
+        for (int mark : marks) {
+            total += mark;
+        }
+        double averagePercentage = (double) total / n;
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("Total Marks: " + total);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+    }
+}
